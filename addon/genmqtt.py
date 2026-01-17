8cad441b6b2707e4cecbe1e0c47e74b283c66e6d
Jeff Terrace <jterrace@gmail.com>
2026-01-17 13:47:40 -0500

    try another timestamp format

--- addon/genmqtt.py
+++ addon/genmqtt.py
@@ -77,7 +77,7 @@ HA_SENSOR_MAP = {
     "generator/Status/Time/Monitor Time": {
         "name": "Genmon Clock",
         "dev_cla": "timestamp",
-        "val_tpl": "{{ as_timestamp(strptime(value, '%A %B %d, %Y %H:%M:%S')) }}",
+        "val_tpl": "{{ strptime(value, '%A %B %d, %Y %H:%M:%S') | as_local | isoformat }}",
         "genmon": True,
     },
     "generator/Maintenance/Controller Settings/Calibrate Current 1": {
